8f5a6a581a39f344488ce1ddf884c5fcb87f337c
Waterstrong <sqlin@thoughtworks.com>
2016-05-02 15:05:02 +0800

    [#TECH][Waterstrong] - change controller return type as ResponseEntity

--- src/main/java/tw/wee/qas/controller/QuestionController.java
+++ src/main/java/tw/wee/qas/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package tw.wee.qas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +18,8 @@ public class QuestionController {
     private QuestionService questionService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/book/{bookUuid}")
-    public Iterable<Question> retrieveQuestions(@PathVariable String bookUuid) {
-        return questionService.retrieveQuestions(bookUuid);
+    public ResponseEntity<?> retrieveQuestions(@PathVariable String bookUuid) {
+        Iterable<Question> questions = questionService.retrieveQuestions(bookUuid);
+        return new ResponseEntity<>(questions, HttpStatus.OK);
     }
 }
