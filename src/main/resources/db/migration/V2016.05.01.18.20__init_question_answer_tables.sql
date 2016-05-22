4542f60038664dbb9c813c17da7bab184723bc9e
Waterstrong <sqlin@thoughtworks.com>
2016-05-22 12:25:50 +0800

    [#TECH][Waterstrong] - use h2 memory database

--- src/main/resources/db/migration/V2016.05.01.18.20__init_question_answer_tables.sql
+++ src/main/resources/db/migration/V2016.05.01.18.20__init_question_answer_tables.sql
@@ -4,8 +4,8 @@ CREATE TABLE wee_question (
   uuid VARCHAR(64) NOT NULL PRIMARY KEY,
   book VARCHAR(64) NOT NULL,
   content VARCHAR(4096) NOT NULL,
-  time_created DATE NOT NULL,
-  last_modified DATE NOT NULL
+  time_created TIMESTAMP NOT NULL,
+  last_modified TIMESTAMP NOT NULL
 );
 
 -- Create answer table
@@ -14,6 +14,6 @@ CREATE TABLE wee_answer (
   uuid VARCHAR(64) NOT NULL PRIMARY KEY,
   question VARCHAR(64) NOT NULL,
   content VARCHAR(4096) NOT NULL,
-  time_created DATE NOT NULL,
-  last_modified DATE NOT NULL
+  time_created TIMESTAMP NOT NULL,
+  last_modified TIMESTAMP NOT NULL
 );
